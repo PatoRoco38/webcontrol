877b928b0bd06e514f3f16f0b8da1c01bdfcaec9
unknown <renato.lancelot@gmail.com>
2025-07-19 18:11:48 -0300

    Criação da autenticação de login

--- core/views.py
+++ core/views.py
@@ -1,8 +1,25 @@
 from django.shortcuts import render
+from django.contrib.auth.decorators import login_required
+from django.contrib.auth import authenticate, login as auth_login
+from django.shortcuts import redirect
 
-# Create your views here.
+
+# View para acessar a página inicial, o @login_required garante que o usuário esteja autenticado
+@login_required
 def index(request):
     return render(request, 'index.html')
 
 def login(request):
+    if request.method == 'POST':
+        username = request.POST.get('username')
+        password = request.POST.get('password')
+        user = authenticate(request, username=username, password=password)
+        
+        if user is not None:
+            auth_login(request, user)
+            return redirect('index')  # Redireciona para a página inicial após o login bem-sucedido
+        else:
+            return render(request, 'login.html', {'error': 'Usuário ou senha inválidos'})
     return render(request, 'login.html')
+
+
